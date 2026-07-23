840dcb711fc058ccffcf9d3a444a39d69c32bd38
Robin Gloaguen E21C116Y <robin.gloaguen@etu.univ-nantes.fr>
2026-07-23 16:33:47 +0200

    Compiling expression

--- src/common.h
+++ src/common.h
@@ -5,6 +5,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define DEBUG_PRINT_CODE
+//When that flag is defined, we use our existing “debug” module to print out the chunk’s bytecode.
 #define DEBUG_TRACE_EXECUTION
 //When this flag is defined, the VM disassembles and prints each instruction right before executing it.
 
