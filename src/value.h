c491ed35e1db3a78ba55116536d5dad591d13d74
Robin Gloaguen E21C116Y <robin.gloaguen@etu.univ-nantes.fr>
2026-07-24 13:39:53 +0200

    Types of Values + correction advance() errorAtC mal placé

--- src/value.h
+++ src/value.h
@@ -3,13 +3,45 @@
 
 #include "common.h"
 
-typedef double Value;
+typedef enum {
+    VAL_BOOL,
+    VAL_NIL,
+    VAL_NUMBER,
+} ValueType;
+
+typedef struct {
+    ValueType type;
+    union {
+    bool boolean;
+    double number;
+    } as;
+} Value;
+
+//Vérifie le type de value
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(value)  ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+
+
+//Retourne la valeur bool ou number, il faut que value est le bon type.
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+/*
+Each one of these takes a C value of the appropriate type and produces a Value
+that has the correct type tag and contains the underlying value
+*/
+#define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 
 typedef struct {
     int capacity;
     int count;
     Value* values;
 } ValueArray;
+
+bool valuesEqual(Value a, Value b);
 
 void initValueArray(ValueArray* array);
 void writeValueArray(ValueArray* array, Value value);
--- src/vm.c
+++ src/vm.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "vm.h"
+#include <stdarg.h>
 #include <stdio.h>
 #include "compiler.h"
 #include "debug.h"
@@ -7,6 +8,26 @@ VM vm;
 
 static void resetStack() {
     vm.stackTop = vm.stack; //La pile est initialisé comme vide
+}
+
+/*
+You’ve certainly called variadic functions—ones that take a varying number of
+arguments—in C before: printf() is one. But you may not have defined your
+own. This book isn’t a C tutorial, so I’ll skim over it here, but basically the ...
+and va_list stuff let us pass an arbitrary number of arguments to
+runtimeError() . It forwards those on to vfprintf() , which is the flavor of
+printf() that takes an explicit va_list .
+*/
+static void runtimeError(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    fputs("\n", stderr);
+    size_t instruction = vm.ip - vm.chunk->code - 1;
+    int line = vm.chunk->lines[instruction];
+    fprintf(stderr, "[line %d] in script\n", line);
+    resetStack();
 }
 
 
@@ -28,6 +49,13 @@ Value pop() {
     return *vm.stackTop;
 }
 
+static Value peek(int distance) {
+    return vm.stackTop[-1 - distance];
+}
+
+static bool isFalsey(Value value) {
+    return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
 
 /*This is the single most important function in all of clox, by far. When the
 interpreter executes a user’s program, it will spend something like 90% of its
@@ -37,11 +65,15 @@ static InterpretResult run() {
     //Comment le ip passe d'une instruction a une autre en sautant les opérandes de certaines ?
 #define READ_BYTE() (*vm.ip++) //The READ_BYTE macro reads the byte currently pointed at by ip and then advances the instruction pointer.
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()]) //Le READ_BYTE lit l'opérande et passe à l'instruction suivante    
-#define BINARY_OP(op) \
+#define BINARY_OP(valueType, op) \
     do { \
-    double b = pop(); \
-    double a = pop(); \
-    push(a op b); \
+    if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
+    runtimeError("Operands must be numbers."); \
+    return INTERPRET_RUNTIME_ERROR; \
+    } \
+    double b = AS_NUMBER(pop()); \
+    double a = AS_NUMBER(pop()); \
+    push(valueType(a op b)); \
     } while (false)
 //le do while(false) permet d'avoir une macro multi instruciton perçu comme une seul qui fonctionne dans un case
 //Car autrement on a un ";" en trop ce qui fera bugguer le switch avec la macro
@@ -68,11 +100,31 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
-            case OP_ADD:        BINARY_OP(+); break;
-            case OP_SUBTRACT:   BINARY_OP(-); break;
-            case OP_MULTIPLY:   BINARY_OP(*); break;
-            case OP_DIVIDE:     BINARY_OP(/); break;
-            case OP_NEGATE:     push(-pop()); break;
+            case OP_NIL: push(NIL_VAL); break;
+            case OP_TRUE: push(BOOL_VAL(true)); break;
+            case OP_FALSE: push(BOOL_VAL(false)); break;
+            case OP_EQUAL: {
+                Value b = pop();
+                Value a = pop();
+                push(BOOL_VAL(valuesEqual(a, b)));
+                break;
+            }
+            case OP_GREATER:    BINARY_OP(BOOL_VAL, >); break;
+            case OP_LESS:       BINARY_OP(BOOL_VAL, <); break;
+            case OP_ADD:        BINARY_OP(NUMBER_VAL, +); break;
+            case OP_SUBTRACT:   BINARY_OP(NUMBER_VAL, -); break;
+            case OP_MULTIPLY:   BINARY_OP(NUMBER_VAL, *); break;
+            case OP_DIVIDE:     BINARY_OP(NUMBER_VAL, /); break;
+            case OP_NOT:
+                push(BOOL_VAL(isFalsey(pop())));
+                break;
+            case OP_NEGATE:
+                if (!IS_NUMBER(peek(0))) { //peek regarde la premiere valeur du stack
+                    runtimeError("Operand must be a number.");
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                push(NUMBER_VAL(-AS_NUMBER(pop())));
+                break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
